0c6c05ec65b621556a31d7bb262c9ab15990fcc1
AlenaShestanova <alenaneew@mail.ru>
2020-05-19 19:07:44 +0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,74 @@
+  $(document).ready(function(){
+      $('.slider').slick({
+      dots: false,
+  infinite: true,
+  
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+   });
+});
+  var anchors = [];
+var currentAnchor = -1;
+var isAnimating  = false;
+$(function(){
+    function updateAnchors() {
+        anchors = [];
+        $('.anchor').each(function(i, element){
+            anchors.push( $(element).offset().top );
+        });
+    }
+    $('body').on('mousewheel', function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        if( isAnimating ) {
+            return false;
+        }
+        isAnimating  = true;
+        if( e.originalEvent.wheelDelta >= 0 ) {
+            currentAnchor--;
+        }else{
+            currentAnchor++;
+        }
+        if( currentAnchor > (anchors.length - 1) 
+           || currentAnchor < 0 ) {
+            currentAnchor = 0;
+        }
+        isAnimating  = true;
+        $('html, body').animate({
+            scrollTop: parseInt( anchors[currentAnchor] )
+        }, 500, 'swing', function(){
+            isAnimating  = false;
+        });
+    });
+    updateAnchors();   
+});
+  
